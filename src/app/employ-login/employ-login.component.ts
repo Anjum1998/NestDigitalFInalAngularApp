88db95cc804bd93bbfc2622e60670ff595164ea3
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-13 14:06:14 +0530

    emplog

---
+++ src/app/employ-login/employ-login.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-employ-login',
+  templateUrl: './employ-login.component.html',
+  styleUrls: ['./employ-login.component.css']
+})
+export class EmployLoginComponent {
+  username=""
+  password=""
+searchUser:any=[]
+constructor(private api:ApiService,private router:Router){}
+readValue=()=>
+  {
+    let data:any= {
+      "username":this.username,"password":this.password
+    }
+    console.log(data)
+    this.api.empLogin(data).subscribe(
+      (response:any)=>
+      {
+        this.username=""
+        this.password=""
+        console.log(response)
+        if (response.status=="success") {
+          this.searchUser=response; 
+          let empid=response.empid
+          console.log(empid)
+          localStorage.setItem("userInfo",empid)
+          this.router.navigate(['/searchemp'])
+        }
+        else{
+          alert(response.message)
+        }
+       
+       
+      }
+    )
+  }
+}
