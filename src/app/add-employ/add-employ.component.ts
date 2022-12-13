9194873fa956fffb522437be20802e1c4c14fdc9
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-13 12:58:05 +0530

    addemploy

--- src/app/add-employ/add-employ.component.ts
+++ src/app/add-employ/add-employ.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-employ',
@@ -12,11 +13,23 @@ export class AddEmployComponent {
   salary=""
   username=""
   password=""
+constructor(private api:ApiService){}
 
   readValue=()=>
   {
     let data:any={"empcode":this.empcode,"empname":this.empname,"designation":this.designation,
     "salary":this.salary,"username":this.username,"password":this.password}
     console.log(data)
+    this.api.addEmploy(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+        if (response.status=="success") {
+          alert("employ added successfully")
+        } else {
+          alert("something wrong")
+        }
+      }
+    )
   }
 }
